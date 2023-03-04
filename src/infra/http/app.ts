afd112f685cc3591eae62765fd464393a6e941f3
Carlos Souza <carlos.pessoal@hotmail.com>
2023-03-04 14:31:31 -0300

    feat: wip web sockets on the browser

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,19 +1,37 @@
+import type { Server } from 'http'
+import type { Express } from 'express'
+
 import { resolve } from 'path'
 import express from 'express'
+import { Server as WebSocketServer } from 'rpc-websockets'
+
 import cookieParser from 'cookie-parser'
 
 import { initializeRoutes } from './routes/index'
 
-export const app = express()
+export const setupServer = (): Express => {
+  const app = express()
+  
+  app.use(cookieParser(process.env.SESSION_SECRET!))
+  app.set('view engine', 'ejs')
+  app.set('views', 'views/pages')
+  
+  app.use('/static', express.static(resolve(__dirname, '../../..', 'static')))
+  
+  app.use(express.urlencoded({
+    extended: true
+  }))
+  
+  initializeRoutes(app)
 
-app.use(cookieParser(process.env.SESSION_SECRET!))
-app.set('view engine', 'ejs')
-app.set('views', 'views/pages')
+  return app
+}
 
-app.use('/static', express.static(resolve(__dirname, '../../..', 'static')))
+export const setupWebSocket = (server: Server) => {
+  const wsServer = new WebSocketServer({ server })
 
-app.use(express.urlencoded({
-  extended: true
-}))
-
-initializeRoutes(app)
+  wsServer.register('sum', (params) => {
+    const [n1, n2] = params as number[]
+    return n1 + n2
+  })
+}
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,7 +1,9 @@
-import { app } from './app'
+import { setupServer, setupWebSocket } from './app'
 
 const port = process.env.PORT || 3333
+const app = setupServer()
 const server = app.listen(port, () => console.log(`Server started at port ${port}`))
+setupWebSocket(server)
 
 const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
 signalTraps.forEach((type) => process.on(type, gracefulShutdown))
